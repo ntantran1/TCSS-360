680dcef676cede66227e2b1557cd127ecdc03c36
kevyen0797 <kevin.nguyen0797@gmail.com>
2020-05-03 18:14:37 -0700

    AboutPage, no Home screen yet

--- TeamLionProject/src/info/AboutPage.java
+++ TeamLionProject/src/info/AboutPage.java
@@ -1,7 +1,58 @@
 package info;
 
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import javax.swing.*;
+
 public class AboutPage {
-	private String[] developers;
-	private String version;
+	String[] developers;
+	String version;
+	String file;
+	
+	public AboutPage() throws IOException {
+		file = "files/";
+		Scanner input = new Scanner(Paths.get(file + "ProjectInfo.txt"));
+		developers = new String[4];
+		getInfo(input);
+	}
+	
+	private void getInfo(Scanner input){
+		version = input.nextLine();
+		for(int i = 0; i < 4; i++) {
+			developers[i] = input.nextLine();
+		}
+	}
+	
+	public void makePage() {
+		var frame = new JFrame();		
+		frame.setLayout(new BorderLayout());
+		JLabel versionText = new JLabel("Version: " + version);
+		JLabel devs = new JLabel("<html>Developers:<br>" +
+				developers[0] + "<br>" +
+				developers[1] + "<br>" +
+ 				developers[2] + "<br>" +
+				developers[3]);
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		panel2.add(devs);
+		
+		JButton home = new JButton("Home");
+		panel3.add(home);
+		
+		panel1.add(versionText);
+		frame.getContentPane().add(panel1, BorderLayout.NORTH);
+		frame.getContentPane().add(panel2, BorderLayout.CENTER);
+		frame.getContentPane().add(panel3, BorderLayout.SOUTH);
+		frame.setVisible(true);
+		frame.setSize(400, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setTitle("About Page");
+	}
+	
 	
 }
---
+++ TeamLionProject/src/pages/Main.java
@@ -0,0 +1,13 @@
+package pages;
+import java.io.IOException;
+
+import javax.swing.*;
+
+import info.AboutPage;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		AboutPage about = new AboutPage();
+		about.makePage();
+	}
+}
