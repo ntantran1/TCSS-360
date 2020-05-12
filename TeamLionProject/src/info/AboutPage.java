aa7ec6b378824e478bd312868c43d907b6f8131d
rabin001 <rabin.gora@gmail.com>
2020-05-11 17:06:42 -0700

    moved the PjojectInfo.txt to the root folder.

--- TeamLionProject/src/info/AboutPage.java
+++ TeamLionProject/src/info/AboutPage.java
@@ -15,11 +15,11 @@ import javax.swing.*;
 public class AboutPage {
 	String[] developers;
 	String version;
-	String file;
+//	String file;
 	
 	public AboutPage() throws IOException {
-		file = "files/";
-		Scanner input = new Scanner(Paths.get(file + "ProjectInfo.txt"));
+//		file = "files/";
+		Scanner input = new Scanner(Paths.get("ProjectInfo.txt"));
 		developers = new String[4];
 		getInfo(input);
 	}
