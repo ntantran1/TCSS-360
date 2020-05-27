50cfbd412ebe80dedcaba2107a1ae17778421f6a
nhan <nhan@LAPTOP-N64KF37E>
2020-05-27 13:48:15 -0700

    deleted the print statement

--- TeamLionProject/src/pages/Main.java
+++ TeamLionProject/src/pages/Main.java
@@ -10,7 +10,7 @@ import info.AboutPage;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		System.out.println("fsdfsdf");
+		
 		AboutPage about = new AboutPage();
 		about.makePage();
 	}
