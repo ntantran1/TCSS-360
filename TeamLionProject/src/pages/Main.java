564086dc5d5c67af594c88adb9d851d2bd863df6
nhan <nhan@LAPTOP-N64KF37E>
2020-05-27 14:16:15 -0700

    ..

--- TeamLionProject/src/pages/Main.java
+++ TeamLionProject/src/pages/Main.java
@@ -13,7 +13,6 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 //		AboutPage about = new AboutPage();
 		HomePage home = new HomePage();
-		//dd
 //		about.makePage();
 	}
 }
