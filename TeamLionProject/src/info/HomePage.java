90682f7cdf974be4a25ae4a4665f67de49336ffd
nhan <nhan@LAPTOP-N64KF37E>
2020-05-27 14:49:06 -0700

    create the about button and added the listener in the home page class

--- TeamLionProject/src/info/HomePage.java
+++ TeamLionProject/src/info/HomePage.java
@@ -1,19 +1,42 @@
 package info;
 
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class HomePage {
 	private static final JFrame frame = new JFrame("Home Page");
 	private AboutPage about;
-	final JButton b = new JButton();
+	JButton aboutButton = new JButton("About Info");
+	JPanel panel = new JPanel();
 	public HomePage() throws IOException{
 		about = new AboutPage();
-		frame.pack();
+		panel.add(aboutButton);
+		frame.setSize(1000, 1000);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().add(panel);		
         frame.setVisible(true);
+        addListener();
 	}
+	
+	 public void addListener() { 
+	        aboutButton.addActionListener(new ActionListener() {
+
+	            @Override
+	            public void actionPerformed(final ActionEvent theE) {
+	                about.makePage();
+	            }
+	                
+	        });
+	} 
+
 
 
 }
