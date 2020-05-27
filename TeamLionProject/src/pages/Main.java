d03eac32bd68a8f8893b379f113692bbb0032788
nhan <nhan@LAPTOP-N64KF37E>
2020-05-27 14:07:07 -0700

    I added a class home page and organized the main page createing the about page will be in the homepage class

--- TeamLionProject/src/pages/Main.java
+++ TeamLionProject/src/pages/Main.java
@@ -7,11 +7,12 @@ import java.io.IOException;
 
 
 import info.AboutPage;
+import info.HomePage;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		
-		AboutPage about = new AboutPage();
-		about.makePage();
+//		AboutPage about = new AboutPage();
+		HomePage home = new HomePage();
+//		about.makePage();
 	}
 }
